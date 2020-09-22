5065005b6dba16d8aef629c300a4f5a137c144c4
Tyler Satkowiak <tylersatkowiak@quickenloans.com>
2020-09-22 09:11:06 -0400

    should be here now

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace TestAPI
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            /*
+            HttpClient client = HttpClient();
+            client.BaseAddress = new Uri("https://catfact.ninja");
+
+
+            var response = client.GetAsync("/fact").Result.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(response);
+            */
+
+
+
+            await test();
+
+        }
+
+        // ASYNCHRONOUS PROGRAMMING
+        public static async Task test()
+        {
+
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://deckofcardsapi.com/");
+
+            var response = await client.GetAsync("api/deck/new/shuffle/?deck_count=1");
+
+            Console.WriteLine(response);
+
+        }
+
+    }
+}
